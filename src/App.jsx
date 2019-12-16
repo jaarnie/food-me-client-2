d6f1c6ad66c9f96318c94f0369906c813f659616
John Arnold <johnarnold@johns-mbp.home>
2019-12-16 22:36:27 +0000

    add sticky header. move navigation and snack/router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import React from "react"
-import { createBrowserHistory } from "history"
-import { Router, Route, Switch } from "react-router-dom"
-import { SnackbarProvider } from "notistack"
+import { Route, Switch } from "react-router-dom"
 import { CssBaseline, Container } from "@material-ui/core"
-// import Axios from "axios"
-// import { configure } from "axios-hooks"
 
-// import { searchRoot, headersRoot } from "./config/apiConfig"
-import Navigation from "./components/Navigation"
 import Home from "./components/Home"
 import SignIn from "./components/user/SignIn.jsx"
 import SignUp from "./components/user/SignUp.jsx"
@@ -15,44 +9,21 @@ import Profile from "./components/user/Profile.jsx"
 import NotFound from "./components/NotFound"
 import RestaurantProfile from "./components/RestaurantProfile"
 
-// const axios = Axios.create({
-//   baseURL: searchRoot,
-//   headers: headersRoot
-// })
-// configure({ axios })
-const browserHistory = createBrowserHistory()
-
-browserHistory.listen((location, action) => {
-  window.scrollTo(0, 0)
-})
-
 function App() {
   return (
     <div className="App">
-      <Router history={browserHistory}>
-        <SnackbarProvider
-          maxSnack={1}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left"
-          }}
-          autoHideDuration={2000}
-        >
-          <CssBaseline />
-          <Navigation />
-          <Container maxWidth="md">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/sign-in" component={SignIn} />
-              <Route path="/sign-up" component={SignUp} />
-              <Route path="/profile" component={Profile} />
-              {/* <Route path="/restaurant/:id" render={(props) => <RestaurantProfile {...props}/>}/> */}
-              <Route path="/restaurant/:id" component={RestaurantProfile} />
-              <Route component={NotFound} />
-            </Switch>
-          </Container>
-        </SnackbarProvider>
-      </Router>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/profile" component={Profile} />
+          {/* <Route path="/restaurant/:id" render={(props) => <RestaurantProfile {...props}/>}/> */}
+          <Route path="/restaurant/:id" component={RestaurantProfile} />
+          <Route component={NotFound} />
+        </Switch>
+      </Container>
     </div>
   )
 }

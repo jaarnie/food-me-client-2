c07b60890e26201bdddf9dbd1ab9cab1c56e43bb
John Arnold <johnadamarnold@gmail.com>
2020-01-02 20:55:09 +0000

    add reviews from new reviews api

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,8 +1,6 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import { GridList, GridListTile, GridListTileBar } from "@material-ui/core/"
-// import IconButton from '@material-ui/core/IconButton';
-// import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 const useStyles = makeStyles(theme => ({
   root: {

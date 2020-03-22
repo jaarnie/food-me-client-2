c33c0ae63cb7a6c927aaa9a1ab1e1676b99b2852
John Arnold <johnadamarnold@gmail.com>
2020-03-22 20:18:51 +0000

    add build folder

---
+++ build/precache-manifest.9e50dd913bdd2c0524b83d5c76085fb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4ffe8d2b54e53c3b570d6c4d50c81a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac601cab8b8e921f0453",
+    "url": "/static/css/main.3f25c60f.chunk.css"
+  },
+  {
+    "revision": "76f4381c4e524a6f4993",
+    "url": "/static/js/0.8eeb787c.chunk.js"
+  },
+  {
+    "revision": "7119bbd3b1a149f26ad5",
+    "url": "/static/js/3.1f2e7ce7.chunk.js"
+  },
+  {
+    "revision": "1de42f07a52bbcc5af99512879a0565b",
+    "url": "/static/js/3.1f2e7ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465318b32c5b977d3fbe",
+    "url": "/static/js/4.fc2be4bf.chunk.js"
+  },
+  {
+    "revision": "ac601cab8b8e921f0453",
+    "url": "/static/js/main.c371db2b.chunk.js"
+  },
+  {
+    "revision": "b834b19f513b5c1d29c9",
+    "url": "/static/js/runtime-main.1b400391.js"
+  }
+]);

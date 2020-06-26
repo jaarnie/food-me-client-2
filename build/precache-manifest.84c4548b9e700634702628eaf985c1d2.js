f4d8361b2be0c263475da8c44b6780d5f0d26c55
John Arnold <johnadamarnold@gmail.com>
2020-06-26 12:20:53 +0100

    add new build

---
+++ build/precache-manifest.84c4548b9e700634702628eaf985c1d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d1e2350779e5f970d18a85c6f0178b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f458d7ae199dfca511fc",
+    "url": "/static/css/main.3f25c60f.chunk.css"
+  },
+  {
+    "revision": "6a6d96ac572cff892e71",
+    "url": "/static/js/0.aef37977.chunk.js"
+  },
+  {
+    "revision": "7119bbd3b1a149f26ad5",
+    "url": "/static/js/3.1f2e7ce7.chunk.js"
+  },
+  {
+    "revision": "1de42f07a52bbcc5af99512879a0565b",
+    "url": "/static/js/3.1f2e7ce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250765a8b88c052bcedc",
+    "url": "/static/js/4.c5ad095d.chunk.js"
+  },
+  {
+    "revision": "f458d7ae199dfca511fc",
+    "url": "/static/js/main.86f2001f.chunk.js"
+  },
+  {
+    "revision": "df26c4a3c11386790e0c",
+    "url": "/static/js/runtime-main.350c9202.js"
+  }
+]);
